0b57c42c16818bd762787c016bf499c04baa110d
tjdgus3160 <tjdgus3160@gmail.com>
2021-03-22 01:11:52 +0900

    Change Profile & Home Controller

---
+++ db.js
@@ -0,0 +1,54 @@
+export const videos = [
+    {
+        id: 324393,
+        title: 'Video awesome',
+        description: 'This is something I love',
+        views: 24,
+        videoFile:
+            'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator: {
+            id: 1212,
+            name: 'ksh',
+            email: 'tjdgus3160@gmail.com',
+        },
+    },
+    {
+        id: 12343,
+        title: 'Video nice',
+        description: 'This is something I love',
+        views: 24,
+        videoFile:
+            'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator: {
+            id: 1212,
+            name: 'ksh',
+            email: 'tjdgus3160@gmail.com',
+        },
+    },
+    {
+        id: 457131,
+        title: 'Video super',
+        description: 'This is something I love',
+        views: 24,
+        videoFile:
+            'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator: {
+            id: 1212,
+            name: 'ksh',
+            email: 'tjdgus3160@gmail.com',
+        },
+    },
+    {
+        id: 89455,
+        title: 'Video perfect',
+        description: 'This is something I love',
+        views: 24,
+        videoFile:
+            'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator: {
+            id: 1212,
+            name: 'ksh',
+            email: 'tjdgus3160@gmail.com',
+        },
+    },
+]
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import routes from '../routes'
+import {
+    changePassword,
+    editProfile,
+    userDetail,
+} from '../controller/userController'
 
 const userRouter = express.Router()
 
-userRouter.get(routes.userDetail, (req, res) => res.send('users'))
-userRouter.get(routes.editProfile, (req, res) => res.send('users'))
-userRouter.get(routes.changePassword, (req, res) => res.send('users'))
+userRouter.get(routes.editProfile, editProfile)
+userRouter.get(routes.changePassword, changePassword)
+userRouter.get(routes.userDetail, userDetail)
 
 export default userRouter
