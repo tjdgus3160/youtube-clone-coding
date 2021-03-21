8dd8c6b5cee553202b183647d68b3fb5eb3e35d5
tjdgus3160 <tjdgus3160@gmail.com>
2021-03-21 12:50:58 +0900

    babel nodemon

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hi')
+import express from 'express'
+const app = express()
+
+const PORT = 4000
+
+const handleListening = () =>
+    console.log(`Listening on: http://localhost:${PORT}`)
+
+const handleHome = (req, res) => res.send('Hello from home!!')
+
+const handleProfile = (req, res) => res.send('You are on my profile')
+
+app.get('/', handleHome)
+
+app.get('/profile', handleProfile)
+
+app.listen(PORT, handleListening)
