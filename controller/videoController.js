c8bb4bf1424c8351ed0cf1a1644b748d84581051
tjdgus3160 <tjdgus3160@gmail.com>
2021-03-22 01:24:09 +0900

    Join Controller

--- controller/videoController.js
+++ controller/videoController.js
@@ -8,7 +8,7 @@ export const search = (req, res) => {
     const {
         query: { term: searchingBy },
     } = req
-    return res.render('search', { pageTitle: 'Search', searchingBy })
+    return res.render('search', { pageTitle: 'Search', searchingBy, videos })
 }
 
 export const upload = (req, res) =>
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,13 +1,15 @@
 import express from 'express'
-import { join, login, logout } from '../controller/userController'
+import { getJoin, postJoin, login, logout } from '../controller/userController'
 import { home, search } from '../controller/videoController'
 import routes from '../routes'
 
 const globalRouter = express.Router()
 
+globalRouter.get(routes.join, getJoin)
+globalRouter.post(routes.join, postJoin)
+
 globalRouter.get(routes.home, home)
 globalRouter.get(routes.search, search)
-globalRouter.get(routes.join, join)
 globalRouter.get(routes.login, login)
 globalRouter.get(routes.logout, logout)
 
