a4cb2eaf03a96db75b76e548ac098af01cb8c84f
tjdgus3160 <tjdgus3160@gmail.com>
2021-03-22 14:04:46 +0900

    Uploading and Creating Video

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,25 +1,38 @@
 import routes from '../routes'
+import Video from '../models/Video'
 
-export const home = (req, res) => {
-    res.render('home', { pageTitle: 'Home', videos })
+export const home = async (req, res) => {
+    try {
+        const videos = await Video.find({})
+        res.render('home', { pageTitle: 'Home', videos })
+    } catch (error) {
+        console.log(error)
+        res.render('home', { pageTitle: 'Home', videos: [] })
+    }
 }
 
 export const search = (req, res) => {
     const {
         query: { term: searchingBy },
     } = req
-    return res.render('search', { pageTitle: 'Search', searchingBy, videos })
+    return res.render('search', { pageTitle: 'Search', searchingBy })
 }
 
 export const getUpload = (req, res) =>
     res.render('upload', { pageTitle: 'Upload' })
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
     const {
-        body: { file, title, description },
+        body: { title, description },
+        file: { path },
     } = req
-    // To Do: Upload and save video
-    res.redirect(routes.videoDetail(324393))
+    const newVideo = await Video.create({
+        fileUrl: path,
+        title,
+        description,
+    })
+    console.log(newVideo)
+    res.redirect(routes.videoDetail(newVideo.id))
 }
 
 export const videoDetail = (req, res) =>
